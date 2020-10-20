23ba290f5c1cb5546eb6eda25fdfb082f92106a3
Bagiukstis <jedziukas14@gmail.com>
2020-10-20 11:01:12 +0200

    modified distance calculator

--- ros1/src/tim551/src/scanner1.cpp
+++ ros1/src/tim551/src/scanner1.cpp
@@ -18,20 +18,12 @@ private:
   float one, two;
   float limit = 0.3;
   float len = 14;
-  //float sum(0);
   std::queue<float> q;
 public:
   void scanner(const sensor_msgs::LaserScan::ConstPtr& scan){
-    //ROS_INFO("Original: [%.5f]",scan->ranges[179]);
     s.ranges = scan->ranges[179];
     ROS_INFO("Original: [%.5f]",s.ranges);
     distance();
-  //  sum += q.back();
-/*    if(q.size()>len){
-      sum -= q.front();
-      sum += q.back();
-    }
-    ROS_INFO("Current SUM: [%.5f]", sum);*/
   }
 
   void distance(){
@@ -40,7 +32,6 @@ public:
         q.push(s.ranges);
         sum += q.back();
         ROS_INFO("Current SUM: [%.5f]", sum);
-        //ROS_INFO("New back: [%.5f]", q.back());
         ROS_INFO("%ld", q.size());
           if(q.size()>len){
           avg = sum/15;
@@ -49,25 +40,12 @@ public:
           ROS_INFO("SUM TOTAL: [%.5f]", sum);
           ROS_INFO("Average: [%.5f]", avg);
           sum -= q.front();
-          //sum = 0;
           q.pop();
-          //ROS_INFO("[%.5f]", q.pop());
-          //ros::shutdown();
         }
       }
-    //  ROS_INFO("%ld", q.size());
       else{
         ROS_INFO("It is too close!");
-        //q.front() = 0;
-        //ROS_INFO("[%.5f]",q.front());
     }
-  /*    while(!q.empty()){
-      ROS_INFO("Ranges: [%.5f]", q.front());
-      sum += q.front();
-      avg = sum/15;
-      q.pop();
-  }*/
-//  ROS_INFO("READING: [%.5f]", s.ranges);
 }
 };
 
@@ -79,10 +57,6 @@ tim551 T;
 ros::Subscriber sub=nh.subscribe("/scan",10,&tim551::scanner, &T);
 T.distance();
 
-
-//tim TC;
-//ros::Subscriber sub=nh.subscribe("/scan",10,scanner);
-//TC.scanner();
 ros::spin();
 return 0;
 }
