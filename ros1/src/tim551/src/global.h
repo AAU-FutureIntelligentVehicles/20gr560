db8058d2b12c6b887fc0d18ee4599b77bc7c2279
Bagiukstis <jedziukas14@gmail.com>
2020-10-20 10:58:30 +0200

    Made a class and a distance calculator.

---
+++ ros1/src/tim551/src/global.h
@@ -0,0 +1,9 @@
+#ifndef TIM551
+#define TIM551
+
+struct scan{
+  float ranges;
+  float angle;
+  float avg;
+};
+#endif
--- ros1/src/tim551/src/scanner1.cpp
+++ ros1/src/tim551/src/scanner1.cpp
@@ -1,23 +1,88 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <sensor_msgs/LaserScan.h>
-/* 270 values in array (1 beam = value)
-*/
-void scanner(const sensor_msgs::LaserScan::ConstPtr& scan){
-  /* 270 values in array (1 beam = value)
-  */
-  ROS_INFO("Range: [%.5f]", scan->ranges[0]);
+#include <queue>
+#include <vector>
+#include "global.h"
+#include <vector>
+#include <numeric>
+#include <string>
+#include <functional>
+// 270 values in array (1 beam = value)
 
-  //ROS_INFO("Angle min: [%.5f]", scan->angle_min);
-  //ROS_INFO("Angle max: [%.5f]", scan->angle_max);
-  //ROS_INFO("Range max: [%.5f]", scan->range_max);
-  //ROS_INFO("Angle inc: [%.5f]", scan->angle_increment);
+class tim551{
+private:
+  scan s;
+  float sum;
+  int i = 0;
+  float one, two;
+  float limit = 0.3;
+  float len = 14;
+  //float sum(0);
+  std::queue<float> q;
+public:
+  void scanner(const sensor_msgs::LaserScan::ConstPtr& scan){
+    //ROS_INFO("Original: [%.5f]",scan->ranges[179]);
+    s.ranges = scan->ranges[179];
+    ROS_INFO("Original: [%.5f]",s.ranges);
+    distance();
+  //  sum += q.back();
+/*    if(q.size()>len){
+      sum -= q.front();
+      sum += q.back();
+    }
+    ROS_INFO("Current SUM: [%.5f]", sum);*/
+  }
+
+  void distance(){
+    float avg;
+      if (s.ranges > limit){
+        q.push(s.ranges);
+        sum += q.back();
+        ROS_INFO("Current SUM: [%.5f]", sum);
+        //ROS_INFO("New back: [%.5f]", q.back());
+        ROS_INFO("%ld", q.size());
+          if(q.size()>len){
+          avg = sum/15;
+          ROS_INFO("First element of the queue: [%.5f]", q.front());
+          ROS_INFO("Last element of the queue: [%.5f]", q.back());
+          ROS_INFO("SUM TOTAL: [%.5f]", sum);
+          ROS_INFO("Average: [%.5f]", avg);
+          sum -= q.front();
+          //sum = 0;
+          q.pop();
+          //ROS_INFO("[%.5f]", q.pop());
+          //ros::shutdown();
+        }
+      }
+    //  ROS_INFO("%ld", q.size());
+      else{
+        ROS_INFO("It is too close!");
+        //q.front() = 0;
+        //ROS_INFO("[%.5f]",q.front());
+    }
+  /*    while(!q.empty()){
+      ROS_INFO("Ranges: [%.5f]", q.front());
+      sum += q.front();
+      avg = sum/15;
+      q.pop();
+  }*/
+//  ROS_INFO("READING: [%.5f]", s.ranges);
 }
+};
+
+
 int main(int argc, char** argv){
 ros::init(argc, argv, "scanner");
 ros::NodeHandle nh;
+tim551 T;
+ros::Subscriber sub=nh.subscribe("/scan",10,&tim551::scanner, &T);
+T.distance();
 
-ros::Subscriber sub=nh.subscribe("/scan",10,scanner);
+
+//tim TC;
+//ros::Subscriber sub=nh.subscribe("/scan",10,scanner);
+//TC.scanner();
 ros::spin();
 return 0;
 }
