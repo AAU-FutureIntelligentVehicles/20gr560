810d0290e722cb11c694dace65c490b3ad97e64f
Bagiukstis <jedziukas14@gmail.com>
2020-10-22 14:45:06 +0200

    added new variables

--- ros1/src/tim551/src/global.h
+++ ros1/src/tim551/src/global.h
@@ -2,8 +2,14 @@
 #define TIM551
 
 struct scan{
-  float ranges;
-  float angle;
-  float avg;
+  float ranges[4];
+  float range;
+  float angle_min;
+  float angle_max;
+  float len = 14;
+  float limit = 0.3;
+};
+struct results{
+  float sum;
 };
 #endif
