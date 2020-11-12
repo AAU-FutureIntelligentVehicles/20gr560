d7728d899997cd285bc4694e66eb617f6f323a44
Bagiukstis <jedziukas14@gmail.com>
2020-11-12 15:33:18 +0100

    Editing avgsub and launch files

--- ros1/src/tim551/src/avgsub.cpp
+++ ros1/src/tim551/src/avgsub.cpp
@@ -1,7 +1,8 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <sensor_msgs/LaserScan.h>
-#include <visualization_msgs/Marker.h>
+//#include <visualization_msgs/Marker.h>
+
 
 class bim{
 private:
@@ -9,7 +10,6 @@ private:
   ros::Subscriber sub;
   ros::Publisher vis_pub;
   sensor_msgs::LaserScan marker;
-  float average[4];
 public:
   void subscriber(){
     sub=nh.subscribe("averages",10,&bim::halo, this);
@@ -18,32 +18,23 @@ public:
     vis_pub = nh.advertise<sensor_msgs::LaserScan>("average_scan", 1000);
   }
   void halo(const sensor_msgs::LaserScan::ConstPtr& msg){
-    average[0] = msg->ranges[0];
-    average[1] = msg->ranges[1];
-    average[2] = msg->ranges[2];
-    average[3] = msg->ranges[3];
-    average[4] = msg->ranges[4];
-    visualization();
-}
-  void visualization(){
-    marker.ranges.resize(5);
-    marker.header.frame_id = "scanneroni";
-    marker.header.stamp = ros::Time();
-    marker.angle_min = -1.570796;
-    marker.angle_max = -1.570796;
-    marker.angle_increment= 0.01745329238474369;
-    marker.time_increment= 0.00018518499564379454;
-    marker.scan_time = 0.06666667014360428;
-    marker.range_min = 0.05000000074505806;
-    marker.range_max = 6;
-
-    marker.ranges[0] = average[0];
-    marker.ranges[1] = average[1];
-    marker.ranges[2] = average[2];
-    marker.ranges[3] = average[3];
-    marker.ranges[4] = average[4];
-    marker.intensities[0];
-    vis_pub.publish(marker);
+      marker.ranges.resize(180);
+     marker.header.frame_id = "mylaser";
+     marker.header.stamp = ros::Time();
+     marker.angle_min = -1.570796;
+     marker.angle_max = -1.570796;
+     marker.angle_increment= 0.01745329238474369;
+     marker.time_increment= 0.00018518499564379454;
+     marker.scan_time = 0.06666667014360428;
+     marker.range_min = 0.05000000074505806;
+     marker.range_max = 8;
+      for(int i=0;i<(msg->ranges.size());i++){
+          if(!(msg->ranges.empty()))
+             // ROS_INFO("%d", i);
+              marker.ranges[i] = msg->ranges[i];
+      }
+      //marker.intensities[0];
+      vis_pub.publish(marker);
 }
 
 };
--- ros1/src/tim551/src/scanner1.cpp
+++ ros1/src/tim551/src/scanner1.cpp
@@ -5,7 +5,7 @@
 #include <limits>
 #include "global.h"
 
-// 270 values in array (1 beam = value)
+
 class tim{
 private:
   scan s;
@@ -26,7 +26,6 @@ void subscriber(){
   sub= nh.subscribe("scan_rear",10,&tim::scanner, this);
 }
 void scanner(const sensor_msgs::LaserScan::ConstPtr& scan){
-//(scan->ranges.size())
   for(int i=0;i<(scan->ranges.size());i++){
             if(!(scan->ranges.empty()))
             s.ranges[i] = scan->ranges[i];
@@ -53,50 +52,7 @@ void distance(int i){
       }
   else{
     ROS_ERROR("***Angle is too close***");
-    //ROS_ERROR("Angle: [%d] deg is too close!", j);
   }
-
-
-
-//************ FOR 5 ANGLES ONLY****************//
-/*  if(i == 0){
-    j = 180;
-  }
-  else if(i == 1){
-    j = 179;
-  }
-  else if(i == 2){
-    j = 178;
-  }
-  else if(i == 3){
-    j = 177;
-  }
-  else if(i == 4){
-    j = 176;
-  }*/
-
-
-/*
-  if (s.ranges[i] > s.limit && s.ranges[i] != inf){
-    q.push(s.ranges[i]);
-    r.sum += q.back();
-      if(q.size()>s.len){
-        msg.ranges.resize(5); // resizing the cpp array to ros array
-        r.avg = r.sum/15;
-        msg.ranges[i] = r.avg;
-        pub.publish(msg);
-        ROS_INFO("Angle: [%d] deg", j);
-        r.lrange[i] = r.avg;
-        ROS_INFO("Average: [%.5f]", r.lrange[i]);
-        r.sum -= q.front();
-        q.pop();
-        }
-      }
-  else{
-    ROS_ERROR("***CRITICAL ERROR***");
-    ROS_ERROR("Angle: [%d] deg is too close!", j);
-  } */
-//****************************************************
 
 
 }
@@ -109,11 +65,3 @@ T.publisher();
 ros::spin();
 return 0;
 }
-
-
-
-//ROS_INFO("First element of the queue: [%.5f]", q.front());
-//ROS_INFO("Last element of the queue: [%.5f]", q.back());
-//  ROS_INFO("SUM TOTAL: [%.5f]", sum);
-//ROS_INFO("Current SUM: [%.5f]", sum);
-//ROS_INFO("%ld", q.size());
