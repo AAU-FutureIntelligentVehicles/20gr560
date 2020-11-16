ed5ae3fe10b2edbad5aa8ba82f36a5002bcb2ff9
frederikjchristensen <frederikjchristensen@gmail.com>
2020-11-16 15:07:13 +0100

    Update teleop

--- workspaces/melodic/src/tpod_teleop/src/tpod_teleop.cpp
+++ workspaces/melodic/src/tpod_teleop/src/tpod_teleop.cpp
@@ -5,42 +5,57 @@ Frederik Johannes Christensen (fjch18@student.aau.dk)
 =======================================
 */
 
+//============= INCLUDES ==============
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
 #include <sstream>
+#include "geometry_msgs/Twist.h"
+#include <iostream>
+//=====================================
 
-
-int main(int argc, char **argv)
-{
-
-  ros::init(argc, argv, "tpod_teleop");
-
-
+class _get_key_teleop{
+public:
+  //Constructor
+  _get_key_teleop() {
+    ros::Subscriber _sub_key_teleop = n.subscribe("key_vel", 10, &_get_key_teleop::KeyCallback, this);
+    ros::Publisher set_vel_pub = n.advertise<std_msgs::Float64>("tpod_set_vel", 1000);
+    ROS_INFO("_get_key_teleop class was constructed");
+  }
+  
+  //Callbacks
+  void KeyCallback(const geometry_msgs::Twist::ConstPtr& msg);
+private:
   ros::NodeHandle n;
+};
+
+void _get_key_teleop::KeyCallback(const geometry_msgs::Twist::ConstPtr& msg){
+  ROS_INFO("CALLBACK RAN");
+}
 
 
-  ros::Publisher set_vel_pub = n.advertise<std_msgs::Float64>("tpod_set_vel", 1000);
-
+int main(int argc, char **argv) //Required inits, allows node to take cmds from externally through terminal etc.
+{
+  ros::init(argc, argv, "tpod_teleop"); //Inits the ros node by name with arguments.
+  ROS_INFO("tpod_teleop initiated");
+  ros::NodeHandle n;
+  _get_key_teleop get_key_teleop;
   ros::Rate loop_rate(10);
 
-  int count = 0;
+
   while (ros::ok())
   {
 
     std_msgs::Float64 set_vel_msg;
-
-    std::stringstream ss;
-    ss << "Set velocity: " << count;
-    set_vel_msg.data = double(count);
+    set_vel_msg.data = double(0.1);
 
     //ROS_INFO("%s", set_vel.data.c_str());
 
-    set_vel_pub.publish(set_vel_msg);
+    //set_vel_pub.publish(set_vel_msg);
+    //get_key_teleop.set_vel_pub.publish(double(0.0));
 
     ros::spinOnce();
 
     loop_rate.sleep();
-    ++count;
   }
 
 
