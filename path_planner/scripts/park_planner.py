468f847bcf0833b0838d61ce28861b914a659c90
Hakaino <marco_alemao@hotmail.com>
2020-11-18 12:15:49 +0100

    can publish poses

--- path_planner/scripts/park_planner.py
+++ path_planner/scripts/park_planner.py
@@ -2,14 +2,39 @@
 
 import rospy, math
 from costmap_converter.msg import ObstacleArrayMsg, ObstacleMsg
-from geometry_msgs.msg import PolygonStamped, Point32
+from geometry_msgs.msg import PolygonStamped, Point32, PoseStamped
+"""
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    
+def listener_pose():
+    rospy.Subscriber("current_position", String, callback)
+    rospy.spin()
+"""
 
+def set_goal():
+    pub = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=1)
+    goal = PoseStamped()
+    
+    goal.header.seq = 1
+    goal.header.stamp = rospy.Time.now()
+    goal.header.frame_id = "map"
+    goal.pose.position.x = 3.0
+    goal.pose.position.y = 3.0
+    goal.pose.position.z = 0.0
+
+    goal.pose.orientation.x = 0.0
+    goal.pose.orientation.y = 0.0
+    goal.pose.orientation.z = 0.0
+    goal.pose.orientation.w = 1.0
+    
+    rospy.sleep(1)
+    pub.publish(goal)
+
+    
 def makebox(ax, ay, bx, by, cx, cy, dx, dy):
-
   pub = rospy.Publisher('/test_optim_node/obstacles', ObstacleArrayMsg, queue_size=1) # /park_planner/lines
   #pub = rospy.Publisher('/p3dx/move_base/TebLocalPlannerROS/obstacles', ObstacleArrayMsg, queue_size=1)
-  rospy.init_node("park_planner")
-
 
   obstacle_msg = ObstacleArrayMsg() 
   obstacle_msg.header.stamp = rospy.Time.now()
@@ -52,8 +77,12 @@ def makebox(ax, ay, bx, by, cx, cy, dx, dy):
 
 
 
-if __name__ == '__main__': 
-  try:
-    makebox(0,0,0,1,1,1,1,0)
-  except rospy.ROSInterruptException:
-    pass
+if __name__ == '__main__':
+    
+    rospy.init_node("park_planner")
+    try:
+        #rospy.Subscriber("current position", Float32MultiArray, callback)
+        #makebox(0,0,0,1,1,1,1,0)
+        set_goal()
+    except rospy.ROSInterruptException:
+        pass
