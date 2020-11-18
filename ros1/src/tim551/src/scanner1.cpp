81ee0d48a06185fed5f58f2704deb4de1ce1f99a
Bagiukstis <jedziukas14@gmail.com>
2020-11-18 10:18:38 +0100

    Removed AVG solution as the measurements were not as accurate/fast as the original

--- ros1/src/tim551/src/scanner1.cpp
+++ ros1/src/tim551/src/scanner1.cpp
@@ -19,13 +19,14 @@ private:
 public:
 
 void publisher(){
-  pub=nh.advertise<sensor_msgs::LaserScan>("averages", 1000);
+  pub=nh.advertise<sensor_msgs::LaserScan>("scan_rear", 1000);
 }
 
 void subscriber(){
-  sub= nh.subscribe("scan_rear",10,&tim::scanner, this);
+  sub= nh.subscribe("scanr",10,&tim::scanner, this);
 }
 void scanner(const sensor_msgs::LaserScan::ConstPtr& scan){
+
   for(int i=0;i<(scan->ranges.size());i++){
             if(!(scan->ranges.empty()))
             s.ranges[i] = scan->ranges[i];
@@ -36,24 +37,25 @@ void scanner(const sensor_msgs::LaserScan::ConstPtr& scan){
 }
 
 void distance(int i){
-  if (s.ranges[i] > s.limit && s.ranges[i] != inf){
-    q.push(s.ranges[i]);
-    r.sum += q.back();
-      if(q.size()>s.len){
-        msg.ranges.resize(180); // resizing the cpp array to ros array
-        r.avg = r.sum/15;
-        msg.ranges[i] = r.avg;
+    msg.header.frame_id = "laser_mount_link";
+    msg.header.stamp = ros::Time();
+    msg.angle_min = -1.570796;
+    msg.angle_max = -1.570796;
+    msg.angle_increment= 0.01745329238474369;
+    msg.time_increment= 0.00018518499564379454;
+    msg.scan_time = 0.06666667014360428;
+    msg.range_min = 0.05000000074505806;
+    msg.range_max = 8;
+    msg.ranges.resize(180);
+    msg.ranges[i] = s.ranges[i];
+    if(s.ranges[i] > s.limit && s.ranges[i] != inf){
         pub.publish(msg);
-        r.lrange[i] = r.avg;
-        ROS_INFO("Average: [%.5f]", r.lrange[i]);
-        r.sum -= q.front();
-        q.pop();
-      }
-      }
-  else{
-    ROS_ERROR("***Angle is too close***");
-  }
-
+    }
+    else{
+        ROS_ERROR("***Angle is too close***");
+       // ros::shutdown();
+    }
+    ROS_INFO("Ranges: [%.5f]", msg.ranges[i]);
 
 }
 };
@@ -65,3 +67,34 @@ T.publisher();
 ros::spin();
 return 0;
 }
+
+
+
+//Old code for average calculation//
+/*
+  msg.header.frame_id = "laser_mount_link";
+  msg.header.stamp = ros::Time();
+  msg.angle_min = -1.570796;
+  msg.angle_max = -1.570796;
+  msg.angle_increment= 0.01745329238474369;
+  msg.time_increment= 0.00018518499564379454;
+  msg.scan_time = 0.06666667014360428;
+  msg.range_min = 0.05000000074505806;
+  msg.range_max = 8;
+if (s.ranges[i] > s.limit && s.ranges[i] != inf){
+  q.push(s.ranges[i]);
+  r.sum += q.back();
+    if(q.size()>s.len){
+      msg.ranges.resize(180); // resizing the cpp array to ros array
+      r.avg = r.sum/15;
+      msg.ranges[i] = r.avg;
+      pub.publish(msg);
+      r.lrange[i] = r.avg;
+      ROS_INFO("Average: [%.5f]", r.lrange[i]);
+      r.sum -= q.front();
+      q.pop();
+    }
+    }
+else{
+  ROS_ERROR("***Angle is too close***");
+} */
