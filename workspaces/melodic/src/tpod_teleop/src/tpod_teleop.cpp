85e5fcc23cfa9cb5b598620e55d310c0e488c49f
frederikjchristensen <frederikjchristensen@gmail.com>
2020-11-20 11:29:50 +0100

    Included odometry to the logger for easier testing

--- workspaces/melodic/src/tpod_teleop/src/tpod_teleop.cpp
+++ workspaces/melodic/src/tpod_teleop/src/tpod_teleop.cpp
@@ -13,13 +13,15 @@ Frederik Johannes Christensen (fjch18@student.aau.dk)
 #include <fstream>
 //=====================================
 double set_vel = 0;
+double odometry_vel = 0;
 
 class _key_teleop{
 public:
   //Constructor
   _key_teleop() {
     _sub_key_teleop = n.subscribe("key_vel", 10, &_key_teleop::KeyCallback, this);
-    set_vel_pub = n.advertise<std_msgs::Float64>("tpod_set_vel", 1000);
+    set_vel_sub = n.subscribe("tpod_set_vel", 1, &_key_teleop::set_velCallback, this);
+    odometry_sub = n.subscribe("odometry", 1, &_key_teleop::odometryCallback, this);
   }
 
   //Callbacks
@@ -27,22 +29,24 @@ public:
     ROS_INFO("Speed: [%f], Turn: [%f]", msg->linear.x, msg->angular.z);
     current_vel = msg->linear.x;
     current_turn = msg->angular.z;
-    set_vel = current_vel;
+    //set_vel = current_vel;
   }
 
-  void _log_vel(){
-
+  void set_velCallback(const std_msgs::Float64::ConstPtr& msg){
+    set_vel = msg->data;
   }
 
-  void _log_turn(){
-
+  void odometryCallback(const std_msgs::Float64::ConstPtr& msg){
+    odometry_vel = msg->data;
   };
 
 
 private:
   ros::NodeHandle n;
   ros::Publisher set_vel_pub;
-  ros::Subscriber _sub_key_teleop;
+  ros::Subscriber _sub_key_teleop,
+                  set_vel_sub,
+                  odometry_sub;
   double current_vel = 0;
   double current_turn = 0;
 };
@@ -59,14 +63,14 @@ int main(int argc, char **argv) //Required inits, allows node to take cmds from 
   ros::Rate loop_rate(10);
   std::ofstream myFile("vel_turn_acc_log.csv");
   myFile << "set_vel,";
-  myFile << "odometry_feedback\n";
+  myFile << "odometry_vel\n";
 
   while (ros::ok())
   {
 
     myFile << set_vel;
     myFile << ",";
-    myFile << "feedback\n";
+    myFile << odometry_vel << '\n';
     ros::spinOnce();
     loop_rate.sleep();
   }
