2f98dfdde1e213bc17277baabf2d713a6f37e8d2
frederikjchristensen <frederikjchristensen@gmail.com>
2020-11-26 13:22:26 +0100

    Created odom static error logger

---
+++ workspaces/melodic/src/loggers/src/static_odometry_error_linear.cpp
@@ -0,0 +1,59 @@
+/*============= AUTHORS ===============
+Frederik Johannes Christensen (fjch18@student.aau.dk)
+20gr560 (5th Semester Robotics AAU)
+=====================================*/
+
+
+//============= INCLUDES ==============
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+#include <sstream>
+#include "nav_msgs/Odometry.h"
+#include <iostream>
+#include <fstream>
+//=====================================
+double x_pos = 0;
+double y_pos = 0;
+
+class logger{
+public:
+  //Constructor
+  logger(){
+    odom_sub = nh.subscribe("odom", 50, &logger::Odom_Callback, this);
+  }
+
+  //Callbacks
+  void Odom_Callback(const nav_msgs::Odometry::ConstPtr& msg){
+    x_pos = msg->pose.pose.position.x;
+    y_pos = msg->pose.pose.position.y;
+  };
+
+private:
+  ros::NodeHandle nh;
+  ros::Subscriber odom_sub;
+};
+
+
+
+
+int main(int argc, char **argv) //Required inits, allows node to take cmds from externally through terminal etc.
+{
+  ros::init(argc, argv, "static_odometry_error_linear_logger"); //Inits the ros node by name with arguments.
+  ros::NodeHandle n;
+  logger logger;
+  ros::Rate loop_rate(10);
+  std::ofstream myFile("static_odometry_error_linear_log.csv");
+  myFile << "x_pos, y_pos" << '\n';
+
+  while (ros::ok())
+  {
+    myFile << x_pos << "," << y_pos << '\n';
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+
+  ROS_INFO("Closing and saving log file");
+  myFile.close();
+  return 0;
+}
