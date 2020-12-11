115d007fb0b0c2b8a556d2800d72f1f400b42820
Bagiukstis <jedziukas14@gmail.com>
2020-12-11 09:40:16 +0100

    Final commit

--- laser_ws/src/laser_data/src/scanner1.cpp
+++ laser_ws/src/laser_data/src/scanner1.cpp
@@ -1,34 +1,32 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <sensor_msgs/LaserScan.h>
-#include <queue>
-#include <limits>
 #include "global.h"
 #include <std_msgs/Float64.h>
-
+#include <algorithm>
 class tim{
 private:
   scan s;
   results r;
   ros::Time newtime;
   ros::NodeHandle nh;
-  ros::Publisher pub, pub2, pub3;
+  ros::Publisher pub, pub2;
   ros::Subscriber sub;
-  std::queue<float> q;
   float inf = std::numeric_limits<float>::infinity();
   sensor_msgs::LaserScan msg;
   std_msgs::Float64 speed, brk;
+  float current_pos = 0;
 
 public:
-
 void publisher(){
   pub=nh.advertise<sensor_msgs::LaserScan>("scan_rear", 1000);
-  pub2=nh.advertise<std_msgs::Float64>("velocity", 1000);
-  pub3=nh.advertise<std_msgs::Float64>("break", 1000);
+  pub2=nh.advertise<std_msgs::Float64>("cmd_accelerator_position", 1000);
 }
-
 void subscriber(){
   sub= nh.subscribe("scanr",10,&tim::scanner, this);
+}
+void subscriber2(){
+    //sub2= nh.subscribe("cmd_accelerator_position", 10, &tim::acceleration, this); //TODO: Subscribe to cmd topic from Golf Control node
 }
 void scanner(const sensor_msgs::LaserScan::ConstPtr& scan){
     newtime = scan->header.stamp;
@@ -48,38 +46,53 @@ void scanner(const sensor_msgs::LaserScan::ConstPtr& scan){
             s.ranges[i] = scan->ranges[i];
           }
   for (int i = 0; i <(scan->ranges.size()); i++) {
-    distance(i);
+    distance(i, current_pos);
   }
 }
-
-void distance(int i){
+/*void acceleration(const std_msgs::Float64::ConstPtr& acc){
+    current_pos = acc->data;
+    //ROS_INFO("Current_pos: [%f]", current_pos);
+    //distance(0,current_pos);
+} *///TODO: Subscribe to cmd topic from Golf Control node
+void distance(int i, float j){
     msg.ranges.resize(180);
     msg.ranges[i] = s.ranges[i];
-    if(s.ranges[i] > s.limit && s.ranges[i] != inf){
-        speed.data = 1;
-        pub2.publish(speed);
-        pub.publish(msg);
-        //ROS_INFO("Angle:[%.5f]", s.ranges[i]);
+    float* min_elem;
+    float speed_setpoint = 0.8; // Manually setting the cmd accelerator position of the car
+    min_elem = std::min_element(s.ranges + 0, s.ranges + 180); // Finding the minimal value of the scan array
+    if(*min_elem > 8 && *min_elem != inf){
+        std::cout<<"Data: "<<*min_elem<<std::endl;
+        speed.data = speed_setpoint * 1;
+        std::cout<<"speed:  "<<speed.data<<std::endl;
     }
-    else{
-  //      ROS_ERROR("***Angle is too close***");
-        if(s.ranges[i] < 0.5 && s.ranges[i] > 0.3 && s.ranges[i] != inf){  //9, 7
-            speed.data = 0.8;
-            pub2.publish(speed);
+    else if(*min_elem < 8 && *min_elem > 6 && *min_elem != inf){  //4,3
+            std::cout<<"*Data: "<<*min_elem<<std::endl;
+            speed.data = speed_setpoint * 0.8;
+            std::cout<<"speed:  "<<speed.data<<std::endl;
         }
-        else if(s.ranges[i] < 0.3 && s.ranges[i] > 0.1 && s.ranges[i] != inf){ //7, 5
-             speed.data = 0.6;
-            pub2.publish(speed);
+    else if(*min_elem < 6 && *min_elem > 4 && *min_elem != inf){ //3, 2
+            std::cout<<"**Data: "<<*min_elem<<std::endl;
+             speed.data = speed_setpoint * 0.6;
+        std::cout<<"speed:  "<<speed.data<<std::endl;
         }
-        else if(s.ranges[i] < 0.1 && s.ranges[i] > 0.0 && s.ranges[i] != inf){ //5, 3
-            speed.data = 0.4;
-            brk.data = 3;
-            pub2.publish(speed);
-            pub3.publish(brk);
+    else if(*min_elem < 4 && *min_elem > 2 && *min_elem != inf){ //2, 1
+            std::cout<<"***Data: "<<*min_elem<<std::endl;
+            speed.data = speed_setpoint * 0.4;
+        std::cout<<"speed:  "<<speed.data<<std::endl;
         }
+    else if(*min_elem < 2 && *min_elem > 1 && *min_elem != inf){ //2, 1
+        std::cout<<"****Data: "<<*min_elem<<std::endl;
+        speed.data = speed_setpoint * 0.2;
+        std::cout<<"speed:  "<<speed.data<<std::endl;
     }
-
-}
+    else if(*min_elem < 1 && *min_elem > 0.05 && *min_elem != inf){ //2, 1
+        std::cout<<"*****Data: "<<*min_elem<<std::endl;
+        speed.data = speed_setpoint * 0;
+        std::cout<<"speed:  "<<speed.data<<std::endl;
+    }
+    pub.publish(msg);
+    pub2.publish(speed);
+    }
 };
 int main(int argc, char** argv){
 ros::init(argc, argv, "scanner");
@@ -89,22 +102,3 @@ T.publisher();
 ros::spin();
 return 0;
 }
-
-
-
-//Old code for average calculation//
-/*
-if (s.ranges[i] > s.limit && s.ranges[i] != inf){
-  q.push(s.ranges[i]);
-  r.sum += q.back();
-    if(q.size()>s.len){
-      msg.ranges.resize(180); // resizing the cpp array to ros array
-      r.avg = r.sum/15;
-      msg.ranges[i] = r.avg;
-      pub.publish(msg);
-      r.lrange[i] = r.avg;
-      ROS_INFO("Average: [%.5f]", r.lrange[i]);
-      r.sum -= q.front();
-      q.pop();
-    }
-    } */
